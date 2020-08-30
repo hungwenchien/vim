2ba31349f182b6a96b1334d9260e9de275517f61
Hungwen Chien <hungwen.chien@goldenrivertek.com>
2020-08-30 16:42:37 +0800

    Add script for install packages

---
+++ install_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install lua5.1 -y
+sudo apt install liblua5.1-dev -y
+sudo apt-get install libx11-dev -y
+sudo apt install libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev -y
